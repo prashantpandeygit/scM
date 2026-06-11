13982236193bf7a7e146fd6e5206788632b256fc
Prashant Pandey <prashantpandeyiuet@gmail.com>
2026-06-12 00:26:38 +0530

    projection headsdef

--- architecture/MoE.py
+++ architecture/MoE.py
@@ -23,3 +23,12 @@ class Encoder(nn.Module):
 
 
 # PROJECTION HEADS
+class ProjectionHead(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.ReLU()
+        )
+    def forward(self, x):
+        return self.net(x)
