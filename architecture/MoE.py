5bd570b80d5edc42f7aeaf16e673bafaf9f270f7
Prashant Pandey <prashantpandeyiuet@gmail.com>
2026-06-11 12:37:03 +0530

    Encoder

---
+++ architecture/MoE.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F 
+import numpy as np 
+import pandas as pd 
+
+# sklearn preprocessing
+
+
+# ENCODER
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim=128, dropout=0.1):
+        super().__init()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 512),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(512, hidden_dim),
+            nn.ReLU()
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
+# PROJECTION HEADS
